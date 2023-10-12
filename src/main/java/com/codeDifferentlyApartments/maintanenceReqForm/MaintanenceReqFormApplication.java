32d5714642245adad65033d3c12e6b0be0a7531e
jacque74 <jacqueann74@gmail.com>
2023-10-11 22:25:51 -0400

    start crud api

---
+++ src/main/java/com/codeDifferentlyApartments/maintanenceReqForm/MaintanenceReqFormApplication.java
@@ -0,0 +1,13 @@
+package com.codeDifferentlyApartments.maintanenceReqForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintanenceReqFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintanenceReqFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeDifferentlyApartments/maintanenceReqForm/MaintanenceReqFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeDifferentlyApartments.maintanenceReqForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintanenceReqFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
